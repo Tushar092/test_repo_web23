49e193c0493809ce883ce1447cf1e79238fbd583
Tushar Singh <karansinghrajora1996@gmail.com>
2022-10-10 21:17:09 +0530

    Added code for Day 5

---
+++ JavaScript - V ( Logical Operators & Ternary Operator ) (IW)/problem7IW.js
@@ -0,0 +1,5 @@
+let char = "p";
+
+if(char != "a" || char != "e" || char != "i" || char != "o" || char != "u"){
+  console.log("Consonant");
+}
---
+++ JavaScript - V ( Logical Operators & Ternary Operator ) (IW)/problem8IW.js
@@ -0,0 +1,5 @@
+let x = 9;
+let y = 20;
+let z = 22;
+
+x > y && x > z ? Console.log("x is greatest") : y > x && y > z ? console.log("y is greatest") : z > x && z > y ? console.log("z is greatest") : console.log("Equal");
